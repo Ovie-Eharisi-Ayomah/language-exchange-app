b4139e12e176edd83d2c67e11d069e9af47ef18d
Ovie Eharisi-Ayomah <dvdhariss@gmail.com>
2024-11-21 13:57:59 +0100

    first commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+    return(
+        <div>
+            <img src="https://robohash.org/test" alt="profile"/>
+            <div>
+                <h2>Name</h2>
+                <h3>Native</h3>
+                <h3>Learning</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
